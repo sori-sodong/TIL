ac3c17de45f75b1f80286aa83d52a75ba6589d1f
김소리 <sorisa4@naver.com>
2023-08-22 23:56:19 +0900

    ✨ 7일차

---
+++ algo/programmers/입문/20.js
@@ -0,0 +1,10 @@
+function solution(my_string, letter) {
+  var answer = '';
+  answer = my_string.replaceAll(letter, '');
+  return answer;
+}
+
+// function solution(my_string, letter) {
+//     const answer = my_string.split(letter).join('')
+//     return answer;
+// }
---
+++ algo/programmers/입문/23.js
@@ -0,0 +1,7 @@
+function solution(n) {
+  var answer = 0;
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 0) answer += i;
+  }
+  return answer;
+}
